ac54fa4e351c8947d9fc7411157bf10f7320d185
Ben Ford <ben.ford@puppetlabs.com>
2016-07-06 14:27:18 -0700

    Restructures to more of a composable library
    See examples/test.rb for usage

---
+++ examples/test.rb
@@ -0,0 +1,31 @@
+#! /usr/bin/env ruby
+require 'learndot'
+require 'learndot/events'
+
+class String
+  def trim(size)
+    if self.size > size
+      "#{self[0...(size - 1)]}…"
+    else
+      self
+    end
+  end
+end
+
+conditions = {
+  'startTime' => [Learndot.timestamp(), Learndot.timestamp(1*7*24*60*60)],
+  'status'    => ['CONFIRMED', 'TENTATIVE'],
+}
+
+ld     = Learndot.new(true)
+events = ld.events.retrieve(conditions)
+
+puts '   Date    |         Course                 |      Location               |  #'
+puts '------------------------------------------------------------------------------'
+events.each do |key, delivery|
+  printf("%10s | %-30s | %-27s | %2s\n",
+      delivery[:start_time],
+      delivery[:course_name].trim(30),
+      delivery[:city],
+      delivery[:enrollment_count])
+end
---
+++ lib/learndot/api.rb
@@ -0,0 +1,131 @@
+require 'httparty'
+require 'json'
+require 'logger'
+
+class Learndot::API
+  attr_writer :logger
+
+  def initialize(token = nil, url = nil, debug = false)
+    @logger           = Logger.new(STDOUT)
+    @logger.level     = debug ? Logger::DEBUG : Logger::WARN
+    @logger.formatter = proc { |level, date, name, msg|
+      "#{level}: #{msg}\n"
+    }
+
+    token ||= get_token
+
+    @base_url = url ? url : "https://learn.puppet.com/api/rest/v2"
+    @headers  = {
+      "TrainingRocket-Authorization"      => token,
+      "Learndot Enterprise-Authorization" => token,
+      "Content-Type" => "application/json",
+      "Accept"       => "application/json; charset=utf-8"
+    }
+  end
+
+  def search(entity, conditions = {}, query = {})
+    endpoint       = "/manage/#{entity}/search"
+    query[:asc]  ||= false
+    query[:or]   ||= false
+
+    if query.include? 'page'
+      api_post(endpoint, conditions, query)
+    else
+      page do |count|
+        query[:page] = count
+        api_post(endpoint, conditions, query)
+      end
+    end
+  end
+
+  def count(entity, conditions = {})
+    endpoint = "/manage/#{entity}/search"
+
+    num_records = api_post(endpoint, conditions)['size']
+    num_records.is_a?(Integer) ? num_records : 0
+  end
+
+  # keep seperate from create to avoid accidental record creation
+  def update(entity, conditions, id)
+    endpoint = "/manage/#{entity}/#{id}"
+    api_post(endpoint, conditions)
+  end
+
+  def create(entity, conditions)
+    endpoint = "/manage/#{entity}"
+    api_post(endpoint, conditions)
+  end
+
+  ############### Private methods ###############
+  def get_token
+    path = File.expand_path('~/.learndot_token')
+
+    File.exists?(path) ? File.read(path).strip : ENV['LEARNDOT_TOKEN']
+  end
+
+  def api_post(endpoint, conditions = {}, query = {})
+    url = @base_url + endpoint
+    @logger.debug "POST: #{url}"
+    @logger.debug "  * Query params: #{query.inspect}"
+    @logger.debug "  * Conditions: #{conditions.inspect}"
+
+
+    response = HTTParty.post(url, {
+      headers: @headers,
+      query: query,
+      body: conditions.to_json,
+    })
+    @logger.debug "#{response.code}: #{response.message}"
+    raise response.message unless response.code == 200
+
+    sleep 1 # dear god learndot
+    response
+  end
+
+  def api_get(endpoint, query = {})
+    url = @base_url + endpoint
+    @logger.debug "GET: #{url}"
+    @logger.debug "  * Query params: #{query.inspect}"
+    @logger.debug "  * Conditions: #{conditions.inspect}"
+
+    response = HTTParty.get(url, { headers: @headers, query: query })
+    @logger.debug "#{response.code}: #{response.message}"
+    raise response.message unless response.code == 200
+
+    sleep 1 # dear god learndot
+    response
+  end
+
+  def hash_response(response)
+    result_hash = {}
+    if response['size'].is_a?(Integer)
+      response['results'].each do | result |
+        result_hash[result['id']] = result
+      end
+    end
+    return result_hash
+  end
+
+  # Call a method provided as a block until there's no more data to get back
+  def page
+    raise 'page() requires a block' unless block_given?
+
+    response    = yield(1)
+    num_records = response['size']
+
+    if num_records.is_a?(Integer) && num_records > 25
+      pages = (num_records / 25) + 1
+      # start at 2 since first call returned first page
+      for counter in 2..pages
+        @logger.debug "Retrieving page #{counter} of #{pages}"
+        results = yield(counter)['results']
+        response['results'].concat(results) if results
+      end
+    end
+
+    hash_response(response)
+  end
+  private :api_post, :api_get, :hash_response, :get_token, :page
+  ############### End private methods ###############
+
+end
